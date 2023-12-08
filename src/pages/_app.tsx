f49f8bec231ffebeb16d52566a318d06d3a8cab5
Jim <limchorngjim@gmail.com>
2023-12-08 14:52:03 +0100

    add Inter font to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Layout from "@app/components/Layout/Layout";
 import "@app/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<main className={inter.className}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</main>
 	);
 }
