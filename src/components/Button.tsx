95996eda8accca4ae919f78a2572f5921cbc9d69
Jim <limchorngjim@gmail.com>
2023-12-08 17:30:10 +0100

    style Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,6 +9,18 @@ const Button: React.FC<Props> = (props) => {
 	return <CustomButton {...props}>{props.children}</CustomButton>;
 };
 
-const CustomButton = styled.button``;
+const CustomButton = styled.button`
+	padding: 8px 12px;
+	border-radius: 6px;
+	border: 1px solid grey;
+	background-color: white;
+	cursor: pointer;
+	transition: 0.22s;
+
+	&:hover {
+		transition: 0.22s;
+		background-color: #f7f7f7;
+	}
+`;
 
 export default Button;
