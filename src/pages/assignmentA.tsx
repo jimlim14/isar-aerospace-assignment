d22f0e7ecd33743213b245622d1d2dc543434e40
Jim <limchorngjim@gmail.com>
2023-12-08 17:29:58 +0100

    style assignmentA page

--- src/pages/assignmentA.tsx
+++ src/pages/assignmentA.tsx
@@ -5,6 +5,7 @@ import { Chart, registerables } from "chart.js";
 import useDetectUserLocale from "@app/hooks/useUserLocale";
 import getTimeFormat from "@app/utils/getTimeFormat";
 import Button from "@app/components/Button";
+import styled from "styled-components";
 
 Chart.register(...registerables);
 
@@ -57,76 +58,93 @@ export default function Home() {
 	const temperatures = getSpectrumData("temperature");
 
 	return (
-		<>
-			<h1>Line chart</h1>
-			<div style={{ display: "flex" }}>
-				<Line
-					datasetIdKey="velocity"
-					data={{
-						labels: [...dates],
-						datasets: [
-							{
-								label: "velocity",
-								data: [...velocities],
-								borderColor: "rgba(75,192,192,1)",
-							},
-						],
-					}}
-				/>
-				<Line
-					datasetIdKey="altitude"
-					data={{
-						labels: [...dates],
-						datasets: [
-							{
-								label: "altitude",
-								data: [...altitude],
-								borderColor: "#5000ff",
-							},
-						],
-					}}
-				/>
-				<Line
-					datasetIdKey="temperature"
-					data={{
-						labels: [...dates],
-						datasets: [
-							{
-								label: "temperature",
-								data: [...temperatures],
-								borderColor: "#00000",
-							},
-						],
-					}}
-				/>
-			</div>
-			{spectrumStatus.length !== 0 && (
-				<>
-					<div>
-						<p>
+		<div style={{ padding: "40px" }}>
+			<h1>Assignment A</h1>
+			<FlexContainer>
+				<FlexItem>
+					<Line
+						datasetIdKey="velocity"
+						data={{
+							labels: [...dates],
+							datasets: [
+								{
+									label: "Velocity",
+									data: [...velocities],
+									borderColor: "rgba(75,192,192,1)",
+								},
+							],
+						}}
+					/>
+				</FlexItem>
+				<FlexItem>
+					<Line
+						datasetIdKey="altitude"
+						data={{
+							labels: [...dates],
+							datasets: [
+								{
+									label: "Altitude",
+									data: [...altitude],
+									borderColor: "#5000ff",
+								},
+							],
+						}}
+					/>
+				</FlexItem>
+				<FlexItem>
+					<Line
+						datasetIdKey="temperature"
+						data={{
+							labels: [...dates],
+							datasets: [
+								{
+									label: "Temperature",
+									data: [...temperatures],
+									borderColor: "#00000",
+								},
+							],
+						}}
+					/>
+				</FlexItem>
+				{spectrumStatus.length !== 0 && (
+					<div style={{padding: "20px"}}>
+						<h3>Other information</h3>
+						<P>
 							latest status message:{" "}
 							{spectrumStatus[spectrumStatus.length - 1].statusMessage}
-						</p>
-					</div>
-					<div>
-						<p>
+						</P>
+						<P>
 							Ascending or Descending:{" "}
 							{spectrumStatus[spectrumStatus.length - 1].isAscending
 								? "Ascending"
 								: "Descending"}
-						</p>
-					</div>
-					<div>
-						<p>
+						</P>
+						<P>
 							Action required:{" "}
 							{spectrumStatus[spectrumStatus.length - 1].isActionRequired
 								? "yes"
 								: "no"}
-						</p>
+						</P>
+						<Button onClick={handleNewDataRequest}>add new data</Button>
 					</div>
-				</>
-			)}
-			<Button onClick={handleNewDataRequest}>add new data</Button>
-		</>
+				)}
+			</FlexContainer>
+		</div>
 	);
 }
+
+export const FlexContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+export const FlexItem = styled.div`
+	position: relative;
+	height: 50vh;
+	min-width: 45vw;
+	max-width: 45vw;
+`;
+
+export const P = styled.p`
+	margin: 10px 0;
+`;
