f08b2891e936f8acac3f3eaa0c2cbd0e8773e2fe
Jim <limchorngjim@gmail.com>
2023-12-11 15:17:02 +0100

    fix websocket component reconnection

--- src/components/WebSocketComponent.tsx
+++ src/components/WebSocketComponent.tsx
@@ -1,54 +1,86 @@
-import { SpectrumWS} from "@app/types/types";
-import { useState, useEffect } from "react";
+import { SpectrumWS } from "@app/types/types";
+import { useState, useEffect, useRef } from "react";
 
 type Props = {
 	onData: (data: SpectrumWS) => void;
 	onOpen: () => void;
 	onClose: () => void;
-	onActionRequired: () => void;
 };
 
 function WebSocketComponent({
 	onData,
 	onOpen,
 	onClose,
-	onActionRequired,
 }: Props) {
 	const spectrumWSApi = process.env.NEXT_PUBLIC_SPECTRUM_WS_API;
 	const [socket, setSocket] = useState<WebSocket | null>(null);
+	const clientRef = useRef<WebSocket | null>(null);
+	const [waitingToReconnect, setWaitingToReconnect] = useState<boolean | null>(
+		null
+	);
+	const [isOpen, setIsOpen] = useState(false);
 
 	useEffect(() => {
+		if (waitingToReconnect) {
+			return;
+		}
+
 		if (!spectrumWSApi) {
 			throw new Error("SPECTRUM_WS_API environment variable is not set.");
 		}
-		const ws = new WebSocket(spectrumWSApi);
 
-		ws.onopen = (event) => {
-			onOpen();
-		};
+		// Only set up the websocket once
+		if (!clientRef.current) {
+			const client = new WebSocket(spectrumWSApi);
+			clientRef.current = client;
 
-		ws.onmessage = (event) => {
-			const data = JSON.parse(event.data);
-			onData(data); // Update UI with live data
+			client.onerror = (e) => console.error(e);
 
-			if (data.isActionRequired) {
-				onActionRequired(); // Handle action required scenario
-			}
-		};
+			client.onopen = () => {
+				onOpen();
+			};
 
-		ws.onclose = () => {
-      onClose();
-			setTimeout(() => {
-				setSocket(new WebSocket(spectrumWSApi));
-			}, 1000); // Reconnect after 1 second
-		};
+			client.onclose = () => {
+				if (clientRef.current) {
+					// Connection failed
+					console.log("ws closed by server");
+				} else {
+					// Cleanup initiated from app side, can return here, to not attempt a reconnect
+					console.log("ws closed by app component unmount");
+					return;
+				}
 
-		setSocket(ws);
+				if (waitingToReconnect) {
+					return;
+				}
 
-		return () => {
-			ws.close(); // Close WebSocket on component unmount
-		};
-	}, [onData, onOpen, onClose, onActionRequired, spectrumWSApi]);
+				// Parse event code and log
+				onClose();
+				console.log("ws closed");
+
+				// Setting this will trigger a re-run of the effect,
+				// cleaning up the current websocket, but not setting
+				// up a new one right away
+				setWaitingToReconnect(true);
+
+				// This will trigger another re-run, and because it is false,
+				// the socket will be set up again
+				setTimeout(() => setWaitingToReconnect(null), 5000);
+			};
+
+			client.onmessage = (message) => {
+        const data = JSON.parse(message.data)
+				onData(data);
+			};
+
+			return () => {
+				// Dereference, so it will set up next time
+				clientRef.current = null;
+
+				client.close();
+			};
+		}
+	}, [onClose, onData, onOpen, spectrumWSApi, waitingToReconnect]);
 
 	return null;
 }
